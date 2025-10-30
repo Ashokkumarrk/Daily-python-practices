e7bab5dbdd61d737045e5f8f6c5444775ea7938b
Ashok Kumar <146415425+Ashokkumarrk@users.noreply.github.com>
2025-10-30 13:14:39 +0530

    Add files via upload

---
+++ Python_logics.py
@@ -0,0 +1,54 @@
+# Reverse a Sentence:
+
+# a=input("enter")                        # user input
+# word=a.split()                          # split word for reverse
+# reversed_word=word[::-1]                # [::-1]  for reverse
+# output=" ".join(reversed_word)          # for join reverse_word
+# print(output)                           # for output
+
+# second largest number in the list:
+
+# b=[10,20,30,40,50,60]
+# b.sort()
+# print(b[-1])   ## first largest
+
+# Pallindrome:
+# name="mada"
+# if name==name[::-1]:
+#     print("Pallindrome")
+# else:
+#     print("not pallindrome")
+
+# count frequency of Each Character:
+
+     # with the use of collections and counter:
+# from collections import Counter
+# print(dict(Counter("ashokkumar")))           
+
+    # With normal python For loops:
+# s=input("enter")
+# count={}
+# for char in s:
+#     if char in count:
+#         count[char]+=1
+#     else:
+#         count[char]=1
+# for k,v in count.items():
+#     print(k," ",v)
+
+# Pattern Printing:
+
+# for i in range(1,6):
+#         print("*" * i)
+
+# Vowels count:
+
+# txt=input("enter")
+# vowels="aeiou"
+# v_count=0
+# for char in txt:
+#     if char in vowels:
+#         v_count +=1
+# print(v_count)
+
+
